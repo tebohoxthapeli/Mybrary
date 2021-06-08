6da7691a9e8dc9923c2f80cda167f446b357a457
Teboho Thapeli <tm.thapeli@gmail.com>
2021-06-08 14:00:51 +0200

    another one

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,22 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // all authors route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', 
+        { authors: authors, searchOptions: req.query })
+    }
+    catch {
+        res.redirect('/')
+    }   
 })
 
 // new author route
@@ -26,7 +40,7 @@ router.post('/', async (req, res) => {
         res.redirect(`authors`)
     }
     catch {
-        res.render('author/new', {
+        res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating author'
         })
--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
-//app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 //app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 
 // ROUTERS:
 
