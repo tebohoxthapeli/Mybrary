9ad8fc9485e2e4803098431b440283bfd4636c9c
Teboho Thapeli <tm.thapeli@gmail.com>
2021-06-12 19:56:16 +0200

    Finished author routes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -19,6 +20,7 @@ app.set('layout', 'layouts/layout')
 
 // MIDDLEWARE:
 
+app.use(methodOverride('_method'))
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
