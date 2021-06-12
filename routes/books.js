1d2371e775f51a45d0efdb0e36150e5f40ea1beb
Teboho Thapeli <tm.thapeli@gmail.com>
2021-06-12 15:29:20 +0200

    a commit

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
 const Book = require('../models/book')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author')
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+// const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) => {
+//         callback(null, imageMimeTypes.includes(file.mimetype))
+//     }
+// })
 
 // all books route 
 
@@ -48,7 +47,7 @@ router.get('/new', async (req, res) => {
 
 // create new book route
 
-router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/', async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/books', bookRouter)
 // DATABASE CONFIG:
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
